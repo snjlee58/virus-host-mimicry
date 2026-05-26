0fa1c81326833e517caa0ab4565c6e0a090e9671
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2026-05-26 13:38:06 +0900

    Edited documentation and added TODOs to run_colabfold script

--- run_localcolabfold.sh
+++ run_localcolabfold.sh
@@ -7,15 +7,17 @@
 #SBATCH --time=04:00:00              # Wall time, adjust to need
 #SBATCH --output=localcolabfold_%j.log    # %j = job ID
 
+# Usage: sbatch run_localcolabfold.sh
+
 # Activate the conda env
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate colabfold
 
-# Optional: redirect weight cache off /home if quota is tight
+# Redirect weight cache of localcolabfold to /fast storage
 export XDG_CACHE_HOME=/fast/sunny
 
-INPUT="virprot1.fasta"
-OUTPUT="virprot1_output"
+INPUT="virprot1.fasta" ## TODO
+OUTPUT="virprot1_output" ## TODO
 
 colabfold_batch \
   --num-recycle 3 \
